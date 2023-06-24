1bb1d20462e98cedf9a33b73448f96926e7656ee
Vairamuthu <mvairamuthu2003@gmail.com>
2023-06-25 01:32:46 +0530

    updated the responsive for job section

--- frontend/src/app/components/CustomFormComp.tsx
+++ frontend/src/app/components/CustomFormComp.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const CustomFormComp = ({ name, type }) => {
   return (
-    <div className="flex flex-col w-full mt-5 md:mt-[50px]">
-      <div className="md:text-[24px] leading-6 font-agrandir pb-3 cursor-pointer">
+    <div className="flex flex-col w-full mt-5">
+      <div className="md:text-[24px] text-white leading-6 font-agrandir pb-3 cursor-pointer">
         {name}
       </div>
       <div>
--- frontend/src/app/components/Jobslist.tsx
+++ frontend/src/app/components/Jobslist.tsx
@@ -16,19 +16,19 @@ export default function Jobslist({
   return (
     <div
       key={id}
-      className="block rounded-xl text-white bg-[#202020] dark:bg-dark-700 p-6 cursor-pointer border border-transparent hover:border-light-400 hover:dark:border-dark-100 w-[900px] mb-[70px]"
+      className=" rounded-xl  text-white bg-[#202020] dark:bg-dark-700 p-6 cursor-pointer border border-white hover:border-yellow-400 hover:dark:border-dark-100 "
     >
-      <div className="flex gap-7 justify-between items-center flex-col md:flex-row w-full  my-[50px]">
+      <div className="flex gap-7 justify-between items-center flex-col md:flex-row w-full py-2 px-5">
         <div className="flex gap-6 items-center w-full">
           <div className="w-full">
             <div className="flex flex-col gap-2">
               <div className="text-xl text-white font-bold line-clamp-1">
                 {title}
               </div>
-              <div className="text-light-300 text-lg font-medium line-clamp-1">
+              <div className="text-light-300 text-md font-medium line-clamp-1">
                 {company} • {location}
               </div>
-              <div className="flex flex-row gap-4 overflow-x-auto w-full md:w-full">
+              <div className="flex flex-row  gap-4 overflow-x-auto w-full md:w-full">
                 <BiSearchAlt2 className="mt-1" />
                 {city}
               </div>
@@ -53,13 +53,14 @@ export default function Jobslist({
         <div className="w-full md:w-fit flex justify-between items-center md:justify-end md:flex-col md:items-end text-white gap-2">
           <div className="whitespace-nowrap text-md text-dark-300 dark:text-light-300">
             <div className="w-fit">
-              <div className="rounded-lg border border-light-400 dark:border-dark-100 flex items-center justify-center h-32 w-32">
+              <div className="rounded-lg  dark:border-dark-100 flex items-center justify-center h-32 w-32">
                 <Image
                   src={logo}
-                  height={250}
-                  width={250}
                   alt="Job Logo"
                   className="rounded-lg"
+                  width={100}
+                  height={100}
+                  style={{ width: "90%", height: "90%" }}
                 />
               </div>
             </div>
