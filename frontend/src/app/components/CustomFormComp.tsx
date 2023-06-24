68682657979eb059ff91c0469c8b3e41684bec91
thirumurugan7 <thiru82003@gmail.com>
2023-06-24 23:29:42 +0530

    explore jobs

--- frontend/src/app/components/CustomFormComp.tsx
+++ frontend/src/app/components/CustomFormComp.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const CustomFormComp = ({ name, type }) => {
   return (
     <div className="flex flex-col w-full mt-[50px]">
-      <div className="text-[24px] leading-6 font-agrandir pb-3 cursor-pointer">
+      <div className="text-[24px] leading-6 font-agrandir pb-3 cursor-pointer text-white">
         {name}
       </div>
       <div>
