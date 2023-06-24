24c26865506521c857280c59e7e42c00087ad081
Vairamuthu <mvairamuthu2003@gmail.com>
2023-06-24 15:36:49 +0530

    updated the Login page

--- frontend/src/app/Login.tsx
+++ frontend/src/app/Login.tsx
@@ -1,16 +1,14 @@
 import React from "react";
+import Logo from "./Images/logo.png";
+import Image from "next/image";
 
-function Signup() {
+function Login() {
   return (
     <div>
       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <img
-            className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-            alt="Workflow"
-          />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          <Image className="mx-auto h-12 w-auto" src={Logo} alt="Workflow" />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-100">
             Sign in to your account
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
@@ -181,4 +179,4 @@ function Signup() {
   );
 }
 
-export default Signup;
+export default Login;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import NavBar from "./components/Navbar";
 import Head from "next/head";
+import Login from "./Login";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <title>My page title</title>
       </Head>
       <NavBar />
+      <Login />
     </main>
   );
 }
