9dd996941d155d3639ca4418e984ff8a87804081
Vairamuthu <mvairamuthu2003@gmail.com>
2023-07-08 11:41:41 +0530

    updated the dashboard components

--- frontend/src/app/CompanyDashboard/page.tsx
+++ frontend/src/app/CompanyDashboard/page.tsx
@@ -1,31 +1,36 @@
 import CustomNav from "../components/customNav";
+import { CgProfile } from "react-icons/cg";
 import { FaHome, FaRunning, FaFile } from "react-icons/fa";
 
 export default function CompanyDashboard() {
   return (
-    <section className="hero3 py-[60px] pb-[100px]">
+    <section className="hero3 py-[95px] pb-[100px]">
       <div>
         <CustomNav />
       </div>
-      <div className="h-screen mt-[100px]">
-        <div className="flex justify-center items-center text-white mid:text-[50px] text-[30px]  px-4 font-agrandir text-center">
-          Hi Nagi! <br /> Let's assist you get the right employee for your
-          company 🚀
+      <div>
+        <div className="flex justify-center items-center text-white md:text-[50px] text-[30px]  px-4 font-agrandir text-center">
+          Hi Team Name ! <br /> Let&apos;s assist you get the empolyee for your
+          comapny 🚀
         </div>
-        <div className="flex justify-center items-center relative md:bottom-0 mid:left-0 md:mx-[100px] my-[35px] rounded-3xl bg-[#0000008F] bg-opacity-50 w-[360px] h-[320px] text-white px-auto">
+        <div className="flex justify-center items-center relative md:bottom-0 md:left-0 md:mx-[100px] my-[35px] rounded-3xl bg-[#0000008F] bg-opacity-50 w-[360px] h-[320px] text-white px-auto">
           <div>
-            <div className="p-[30px] flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
+            <div className="py-3 flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
               <FaHome className="w-8 h-8" />
               Home
             </div>
-            <div className="p-[30px] flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
+            <div className="py-3 flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
               {" "}
               <FaRunning className="w-8 h-8" />
               Create Post
             </div>
-            <div className="p-[30px] flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
+            <div className="py-3 flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
               <FaFile className="w-8 h-8" />
               Past Post Applications
+            </div>
+            <div className="py-3 flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
+              <CgProfile className="w-8 h-8" />
+              Profile
             </div>
           </div>
         </div>
--- frontend/src/app/UserDashboard/page.tsx
+++ frontend/src/app/UserDashboard/page.tsx
@@ -1,30 +1,34 @@
 import CustomNav from "../components/customNav";
 import { FaHome, FaRunning, FaFile } from "react-icons/fa";
-
+import { CgProfile } from "react-icons/cg";
 export default function UserDashboard() {
   return (
-    <section className="hero3 py-[60px] pb-[100px]">
+    <section className="hero3 py-[95px] pb-[100px]">
       <div>
         <CustomNav />
       </div>
-      <div className="h-screen mt-[100px]">
-        <div className="flex justify-center items-center text-white mid:text-[50px] text-[30px] font-agrandir text-center">
+      <div>
+        <div className="flex justify-center items-center text-white md:text-[50px] text-[30px] font-agrandir text-center">
           Hi Nagi! <br /> Lets assist you to get the job of your dream 🚀
         </div>
-        <div className="flex justify-center items-center relative md:bottom-0 mid:left-0 md:mx-[100px] my-[35px] rounded-3xl bg-[#0000008F] bg-opacity-50 w-[360px] h-[320px] text-white px-auto">
+        <div className="flex justify-center items-center relative md:bottom-0 md:left-0 md:mx-[100px] my-[35px] rounded-3xl bg-[#0000008F] bg-opacity-50 w-[360px] h-[320px] text-white ">
           <div>
-            <div className="p-[30px] flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
+            <div className="py-3 flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
               <FaHome className="w-8 h-8" />
               Home
             </div>
-            <div className="p-[30px] flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
+            <div className="py-3  flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
               {" "}
               <FaRunning className="w-8 h-8" />
               Jobs
             </div>
-            <div className="p-[30px] flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
+            <div className=" py-3 flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
               <FaFile className="w-8 h-8" />
               My Applications
+            </div>
+            <div className="py-3 flex text-center text-[22px] leading-6 font-agrandir justify-start items-center gap-4 cursor-pointer">
+              <CgProfile className="w-8 h-8" />
+              Profile
             </div>
           </div>
         </div>
