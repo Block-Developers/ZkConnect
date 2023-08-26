0ce7a02652774d3d7a26be42df79a9c78a32c074
Vairamuthu <mvairamuthu2003@gmail.com>
2023-08-26 16:06:47 +0530

    update with icon test in the dashboard

--- frontend/src/app/components/DashNav.tsx
+++ frontend/src/app/components/DashNav.tsx
@@ -1,26 +1,24 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { Dialog } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/outline"; // Corrected the import path
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"; // Corrected the import path
 import Image from "next/image";
 import Link from "next/link";
 import { FiUser } from "react-icons/fi";
 import Logo from "../Images/logo.png";
 import { getLocalStorageWithExpiry } from "../components/store";
 import { handleLogout } from "../components/store"; // Import the handleLogout function
-import { useRouter } from "next/router"; // Import the useRouter from the correct path
+import { useRouter } from "next/navigation";
 
-const DashNav: React.FC = () => {
+const DashNav = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [userdata, setUserdata] = useState<{ username?: string } | null>(null);
   const [showDropdown, setShowDropdown] = useState(false); // State to manage the dropdown
   const router = useRouter(); // Initialize the router
-
   useEffect(() => {
     const retrievedValue = getLocalStorageWithExpiry("userId");
     setUserdata(retrievedValue?.user);
   }, []);
-
   const logout = () => {
     handleLogout("userId"); // Remove the userId from localStorage
     setUserdata(null); // Update the component state
@@ -109,7 +107,53 @@ const DashNav: React.FC = () => {
           open={mobileMenuOpen}
           onClose={setMobileMenuOpen}
         >
-          {/* ...dialog content */}
+          <div className="fixed inset-0 z-50" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 bg-black z-50 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <div className="flex">
+                  <Image className="h-16 w-auto" src={Logo} alt="" />
+                  <p className="text-white text-xl font-bold p-4">ZKConnect</p>
+                </div>
+              </a>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-100"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon
+                  className="h-12 w-17 border rounded-xl p-2 border-white"
+                  aria-hidden="true"
+                />
+              </button>
+            </div>
+            <div className="mt-7 flow-root">
+              <div className="-my-6 divide-y gray-100/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-800"
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
+                <div className="py-6">
+                  <Link
+                    href="/Login"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-800"
+                  >
+                    <button className="border  rounded-xl	 border-white p-3 ">
+                      LOGIN/SIGN IN
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
         </Dialog>
       </header>
     </div>
